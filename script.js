69a816ad4a12d6764d5948ab97f2c30b501f318a
Raushan <raushan.kumar.cer20@itbhu.ac.in>
2024-01-21 21:50:10 +0530

    changes done

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const clock = document.getElementById("clock");
 const clock2 = document.getElementById("clock2");
-
-
-
-
-
 setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
